51f60c2bdc7dd79e420f9905a49bb789c991398f
robertelias <robert.elias@live.com>
2020-05-25 21:54:03 -0700

    model updated

--- tickets/tickets-model.js
+++ tickets/tickets-model.js
@@ -1,9 +1,24 @@
 const db = require('../data/dbConfig.js');
 module.exports = {
+    add,
+    addToStudent,
     find,
-    findBy
+    findBy,
+    findById
     
 };
+
+async function add(ticket) {
+    return await db('tickets')
+        .insert(ticket, 'id')
+        .then(([id]) => findById(id));
+}
+
+function addToStudent(studentid, ticketid) {
+    return db('stud_tickets')
+        .insert({ studentid, ticketid }, 'id')
+        .then(() => findById(ticketid));
+}
 
 function find(filter) {
     return db('tickets').where(filter);
@@ -13,3 +28,9 @@ function findBy() {
     return db('tickets');
 }
 
+function findById(id) {
+    return db('tickets')
+        .select('id', 'title', 'description', 'solution')
+        .where({ id })
+        .first();
+}
