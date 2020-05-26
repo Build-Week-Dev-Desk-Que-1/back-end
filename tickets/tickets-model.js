7a96432bb1e164f7875ac8a54a6cdce302d2e185
robertelias <robert.elias@live.com>
2020-05-25 21:50:34 -0700

    find() completed

--- tickets/tickets-model.js
+++ tickets/tickets-model.js
@@ -1,11 +1,15 @@
 const db = require('../data/dbConfig.js');
 module.exports = {
-   
+    find,
     findBy
     
 };
 
+function find(filter) {
+    return db('tickets').where(filter);
+}
 
 function findBy() {
     return db('tickets');
 }
+
