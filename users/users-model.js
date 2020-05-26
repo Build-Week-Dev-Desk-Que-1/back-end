3cdf7040ab613757a11dbcbf945e7b73cc333d92
robertelias <robert.elias@live.com>
2020-05-25 17:32:37 -0700

    findBy() completed

--- users/users-model.js
+++ users/users-model.js
@@ -1,6 +1,7 @@
 const db = require("../data/dbConfig.js");
 module.exports = {
-    add
+    add,
+    findBy
 
 };
 
@@ -8,4 +9,9 @@ function add(user) {
     return db('users').insert(user, "id").then(ids => { const [id] = ids; return findById(id); }).catch(error => {
         return res.status(500).json({ message: 'failed to add new user' });
     });
+}
+
+
+function findBy(filter) {
+    return db("users").where(filter);
 }
